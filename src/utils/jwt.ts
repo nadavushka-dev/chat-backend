4dfab220ab8d646df03bd33a3fb15a138d83eaf9
nadavushka-dev <nadavushkadev@gmail.com>
2025-12-09 18:48:28 +0200

    utils folder

---
+++ src/utils/jwt.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import config from "../config";
+
+export type JwtPayloadT = {
+  userId: number;
+  iat?: number;
+  exp?: number;
+};
+
+function jwtEncode(payload: JwtPayloadT): string {
+  return jwt.sign(payload, config.secret, { expiresIn: "14d" });
+}
+
+function jwtVerify(token: string): JwtPayloadT {
+  const payload = jwt.verify(token, config.secret);
+  if (typeof payload === "string") {
+    throw new Error("Invalid token payload");
+  }
+
+  return payload as JwtPayloadT;
+}
+
+export { jwtEncode, jwtVerify };
