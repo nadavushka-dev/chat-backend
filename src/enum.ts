2954fca85b4eabc28b6df9faa5d86d4ed246efc0
nadavushka-dev <nadavushkadev@gmail.com>
2025-12-09 18:48:35 +0200

    enum file

---
+++ src/enum.ts
@@ -0,0 +1,10 @@
+export const enum SE {
+  CONNECT = "connect",
+  CONNECTION = "connection",
+  DISCONNECT = "disconnect",
+  DELIVER = "deliver",
+  JOIN_ROOM = "join_room",
+  LEAVE_ROOM = "leave_room",
+  MSG = "msg",
+  ERROR = "error",
+}
